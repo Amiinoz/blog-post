1913bf6364b2702253490044110e97bf410814ed
Amiinoz <momagan@gmail.com>
2021-03-10 12:38:07 -0700

    finishing pages and links

---
+++ src/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Contact = () => {
+
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>My info</p>
+    </div>
+
+  )
+}
+
+export default Contact
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h3>I am Mohamed a web developer based in Calgary, Canada</h3>
+      <p>Need a developer? <Link to="/Contact"> Contact me.</Link></p>
+
+    </div>
+
+  )
 }
+
+export default IndexPage

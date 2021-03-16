83dab14f860e3fe696a81029e90247d09b079d44
Amiinoz <momagan@gmail.com>
2021-03-15 22:09:16 -0600

    finished Dynamically Generating Pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,7 @@ module.exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === 'MarkdownRemark') {
-    const slug = path.basename(node.fileAbsolutePath, 'md')
+    const slug = path.basename(node.fileAbsolutePath, '.md')
 
     // console.log(node)
     //console.log(JSON.stringify(node, undefined, 4))
@@ -20,6 +20,43 @@ module.exports.onCreateNode = ({ node, actions }) => {
     })
 
   }
+
+
+}
+
+// Creating dynamic blog pages
+// 1. get path to template
+// 2. get markdown data
+// 3. create new page
+
+module.exports.createPages = async ({ graphql, actions }) => {
+
+  const { createPage } = actions
+  const blogTemplate = path.resolve('./src/templates/blog.js')
+  const res = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+  res.data.allMarkdownRemark.edges.forEach((edge) => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug
+      }
+    })
+  })
+
+
 
 
 }
